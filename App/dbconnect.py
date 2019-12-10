e96f6075d27ada89fcebca5918dc84e192d34569
mkan <36687256+muskanasmath@users.noreply.github.com>
2019-12-10 16:43:14 +0530

    Update dbconnect.py

--- App/dbconnect.py
+++ App/dbconnect.py
@@ -2,7 +2,7 @@ import MySQLdb
 
 HOST     = 'localhost'
 USER     = 'root'
-PASSWD   = 'Asmath.123'
+PASSWD   = 'password'
 DATABASE = 'society_mysqldb'
 def connection():
 	conn = MySQLdb.connect(host = HOST, user = USER, passwd = PASSWD, db = DATABASE)
